c0d8ce6a91d0bc13feeea5376b7d8e1430ac3994
2dam <2dam@HZ375751.TartangaLH.eus>
2021-09-14 10:01:51 +0200

    First version with clases

---
+++ src/clases/Customer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Customer {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+    private int phone;
+    private String email;
+    
+    
+    
+}
